09f715f723d017c6dec0b626744b0bff2cfef7fc
hannahannx <4HACKH44@solent.ac.uk>
2024-02-29 15:15:29 +0000

    week 1 -3

--- week3/public/addSong.js
+++ week3/public/addSong.js
@@ -1,19 +1,31 @@
 //add song ajax function
+
+/*Add a new HTML page containing a form to allow the user to add a new song.
+ When the user clicks the button on the form, 
+ an AJAX POST request should be sent to your "add song" route from Week 2.*/
+
 async function ajaxAdd(newSong){
     try{
         //sending the request to the localhost URL 
-        const response = await fetch('http://localhost:3300/songs/addnewsong/', {
-        //read in the parameters artist title etc n add to database    
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body:JSON.stringify(newSong)
+        const response = await fetch('http://localhost:3300/songs/addnewsong', {
+
+             //read in the parameters artist title etc n add to database    
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body:JSON.stringify(newSong)//converting the JSON string fromt his route into a JSON string
         });
-    }catch (e) {
-        alert(`Error ${e}`);
+        //checking the type of error message for the server an returning the correct code
+        if (response.status = 400){
+            alert("One or more of your fields are blank")
+        }else{
+            alert(`Added new song to database`)
+        }
+    }catch(error) {
+        alert(`Error ${error.message}`);
     }
-}
+};
 
 //AJAX running to add to database when sumbit button pressed
 document.getElementById('addNewSong').addEventListener('click', ()=> {
@@ -34,4 +46,6 @@ document.getElementById('addNewSong').addEventListener('click', ()=> {
     }
 
     ajaxAdd(newSong)
-})
+});
+
+//
